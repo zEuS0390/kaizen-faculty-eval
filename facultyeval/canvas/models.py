0d07c0ee0c4a2bcfcdf4d4208a83df6a34c69f77
zEuS-0390 <zeusjamesbaltazar@gmail.com>
2022-03-28 07:15:30 +0800

    Add comments to some models in canvas app

--- facultyeval/canvas/models.py
+++ facultyeval/canvas/models.py
@@ -3,6 +3,10 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Member(models.Model):
+    """
+    The member model is the faculty member that is also attached to user account.
+    This will allow them to view their evaluations by visiting the web application.
+    """
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=200)
     middle_name = models.CharField(max_length=200)
@@ -11,12 +15,19 @@ class Member(models.Model):
         return f"{self.last_name}, {self.first_name}, {self.middle_name}"
 
 class Course(models.Model):
+    """
+    The course model will specify the area of performance of the faculty member.
+    """
     title = models.CharField(max_length=200)
     code = models.CharField(max_length=200)
     def __str__(self):
         return f"{self.code} - {self.title}"
 
 class Section(models.Model):
+    """
+    The section model determines the specific class in the course that
+    the evaluation of a faculty member has been gathered.
+    """
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     section_name = models.CharField(max_length=200)
     def __str__(self):
