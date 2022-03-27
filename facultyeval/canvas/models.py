b3ed09bd55c7742f7d09d6cf017357ef06d0836d
zEuS-0390 <zeusjamesbaltazar@gmail.com>
2022-03-28 06:18:23 +0800

    Add item table for holding criteria descriptions

--- facultyeval/canvas/models.py
+++ facultyeval/canvas/models.py
@@ -11,10 +11,10 @@ class Member(models.Model):
         return f"{self.last_name}, {self.first_name}, {self.middle_name}"
 
 class Course(models.Model):
-    course_title = models.CharField(max_length=200)
-    course_code = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    code = models.CharField(max_length=200)
     def __str__(self):
-        return f"{self.course_code} - {self.course_title}"
+        return f"{self.code} - {self.title}"
 
 class Section(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
@@ -23,26 +23,31 @@ class Section(models.Model):
         return f"{self.section_name} - {self.course}"
 
 class Result(models.Model):
-    faculty_member = models.ForeignKey(Member, on_delete=models.CASCADE)
-    faculty_course = models.ForeignKey(Course, on_delete=models.CASCADE)
-
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
     def __str__(self):
-        return f"{self.faculty_course} - {self.faculty_member}"
+        return f"{self.course} - {self.member}"
 
 class Evaluation(models.Model):
-    evaluation_title = models.CharField(max_length=200, null=True)
+    title = models.CharField(max_length=200, null=True)
     def __str__(self):
-        return f"{self.evaluation_title}"
+        return f"{self.title}"
 
 class Criteria(models.Model):
-    faculty_evaluation = models.ForeignKey(Evaluation, on_delete=models.CASCADE)
-    criteria_title = models.CharField(max_length=200)
+    evaluation = models.ForeignKey(Evaluation, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
     def __str__(self):
-        return f"{self.faculty_evaluation} - {self.criteria_title}"
+        return f"{self.evaluation} - {self.title}"
+
+class Item(models.Model):
+    criteria = models.ForeignKey(Criteria, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200)
+    def __str__(self):
+        return f"{self.criteria} - {self.description}"
 
 class CriteriaScore(models.Model):
-    evaluation_result = models.ForeignKey(Result, on_delete=models.CASCADE)
-    evalutation_criteria = models.ForeignKey(Criteria, on_delete=models.CASCADE)
+    result = models.ForeignKey(Result, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
     score = models.PositiveIntegerField(default=0)
     def __str__(self):
-        return f"{self.evaluation_result} - {self.evalutation_criteria} - {self.score}"
+        return f"{self.result} - {self.item.description} - {self.score}"
