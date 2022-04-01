778aa03e3b82e60f4054f0bf59b0341c43f2cbd7
zEuS-0390 <zeusjamesbaltazar@gmail.com>
2022-04-01 23:44:02 +0800

    Set up media files

--- facultyeval/canvas/urls.py
+++ facultyeval/canvas/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import Index
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = "canvas"
 urlpatterns = [
--- facultyeval/facultyeval/settings.py
+++ facultyeval/facultyeval/settings.py
@@ -129,6 +129,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
