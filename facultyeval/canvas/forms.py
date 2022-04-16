57c777cca5ebc576727a5e4104c7eecad2b12a10
zEuS-0390 <zeusjamesbaltazar@gmail.com>
2022-04-16 17:38:12 +0800

    Implement basic CRUD for evaluation performance in LMS

--- facultyeval/canvas/forms.py
+++ facultyeval/canvas/forms.py
@@ -1,14 +1,14 @@
 from django import forms
 from .models import MGRating
 
-class CreateEvaulation(forms.ModelForm):
+class EvaluationForm(forms.ModelForm):
 
     class Meta:
         model = MGRating
         fields = "__all__"
 
     def __init__(self, *args, **kwargs):
-        super(CreateEvaulation, self).__init__(*args, **kwargs)
+        super(EvaluationForm, self).__init__(*args, **kwargs)
         for field in self.fields:
             self.fields[field].required = True
             self.fields[field].widget.attrs.update({"class":"form-control"})
--- facultyeval/canvas/views.py
+++ facultyeval/canvas/views.py
@@ -4,12 +4,16 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from .decorators import admin_only
-from .forms import CreateEvaulation
+from .forms import EvaluationForm
 from administrator.models import SchoolYear
 from .models import MGRating
 
 # Create your views here.
 class Index(View):
+    """
+    Index is the first view of the LMS (Canvas), it consist basic implementation 
+    of CRUD for accessing the application's database.
+    """
 
     @method_decorator(login_required(login_url="accounts:login"))
     @method_decorator(admin_only)
@@ -23,21 +27,24 @@ class Index(View):
     def post(self, request):
         return redirect("/")
 
-class EvaluationFormEntry(View):
+class CreateEval(View):
+    """
+    This view will create a performance evaluation of LMS (Canvas)
+    for a specified faculty member that exist within the database.
+    """
 
     @method_decorator(login_required(login_url="accounts:login"))
     @method_decorator(admin_only)
     def get(self, request):
-        form = CreateEvaulation()
-        return render(request, template_name="canvas/evaluation_form_entry.html", context={"form": form})
+        form = EvaluationForm()
+        return render(request, template_name="canvas/evaluation_form.html", context={"form": form})
 
     @method_decorator(login_required(login_url="accounts:login"))
     @method_decorator(admin_only)
     def post(self, request):
-        form = CreateEvaulation(request.POST)
+        form = EvaluationForm(request.POST)
         if form.is_valid:
             mgrating = form.save(commit=False)
-            # Check if the evaluation form entry already exist
             member = form.cleaned_data.get("member")
             school_year = form.cleaned_data.get("school_year")
             group_title = form.cleaned_data.get("group_title")
@@ -49,11 +56,60 @@ class EvaluationFormEntry(View):
                 messages.error(request, "Evaluation form entry already exist!")
                 return redirect("canvas:eval_entry")
             mgrating.save()
+            messages.success(request, "Evaluation successfully created!")
             return redirect("canvas:index")
         return redirect("/")
+
+class EditEval(View):
+    """
+    This view will edit an existing performance evaluation
+    of a specified faculty member using ID attribute in 
+    LMS's (Canvas) MGRating table.
+    """
+
+    @method_decorator(login_required(login_url="accounts:login"))
+    @method_decorator(admin_only)
+    def get(self, request, ID):
+        result = MGRating.objects.filter(id=ID)
+        # Check if the performance evaluation already exist
+        if result.exists():
+            selected_mgrating = result.first()
+            form = EvaluationForm({
+                "member": selected_mgrating.member,
+                "school_year": selected_mgrating.school_year,
+                "group_title": selected_mgrating.group_title,
+                "semester": selected_mgrating.semester,
+                "part1": selected_mgrating.part1,
+                "part2": selected_mgrating.part2,
+                "final": selected_mgrating.final,
+                "remarks": selected_mgrating.remarks
+            })
+            return render(request, template_name="canvas/evaluation_form.html", context={"form":form})
+        messages.error(request, f"ID {ID} does not exist!")
+        return redirect("canvas:index")
+
+    @method_decorator(login_required(login_url="accounts:login"))
+    @method_decorator(admin_only)
+    def post(self, request, ID):
+        form = EvaluationForm(request.POST)
+        if form.is_valid():
+            form.save(commit=False)
+            selected_mgrating = MGRating.objects.filter(id=ID)
+            selected_mgrating.update(**form.cleaned_data)
+        return redirect("canvas:index")
 
 @login_required(login_url="accounts:login")
 @admin_only
 def DeleteEval(request, ID):
-    MGRating.objects.filter(id=ID).delete()
+    """
+    This view will delete the performance evaluation of a specified
+    faculty member using ID attribute in LMS's (Canvas) MGRating table.
+    """
+    result = MGRating.objects.filter(id=ID)
+    # Check if the performance evaluation already exist
+    if result.exists():
+        result.delete()
+        messages.success(request, f"ID {ID} has been successfully removed")
+        return redirect("canvas:index")
+    messages.error(request, f"ID {ID} does not exist!")
     return redirect("canvas:index")
