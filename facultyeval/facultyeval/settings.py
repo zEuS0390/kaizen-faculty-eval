c10ef21b75c65f53b970cb90035e3ff137127f3d
crebello26 <crebellocynnamae@gmail.com>
2022-04-19 00:14:51 +0800

    Add email notif for admins after reg

--- facultyeval/facultyeval/settings.py
+++ facultyeval/facultyeval/settings.py
@@ -133,6 +133,8 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
+ADMINS = []
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
