fdb43378116b22459c65a9b7a46f559593b1cc4b
ianmarquez1129 <ianmarquez1129@gmail.com>
2022-04-24 21:23:53 +0800

    Add URL filter in LMS

--- facultyeval/canvas/urls.py
+++ facultyeval/canvas/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 app_name = "canvas"
 urlpatterns = [
-    path("", Index.as_view(), name="index"),
+    path("<slug:SEM>/<str:MG>/<slug:SY>/", Index.as_view(), name="index"),
     path("evaluation-form-entry/", CreateEval.as_view(), name="eval_entry"),
     path("edit-evaluation/<int:ID>/", EditEval.as_view(), name="edit_eval"),
     path("delete-evaluation/<int:ID>/", DeleteEval, name="delete_eval")
--- facultyeval/canvas/views.py
+++ facultyeval/canvas/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.views import View
 from django.contrib import messages
@@ -18,9 +19,11 @@ class Index(View):
 
     @method_decorator(login_required(login_url="accounts:login"))
     @method_decorator(admin_only)
-    def get(self, request):
-        school_year = SchoolYear.objects.filter(school_year="2021-2022").first()
-        ratings = MGRating.objects.filter(school_year=school_year, group_title="MG1", semester="1st Sem")
+    def get(self, request, SEM, MG, SY):
+        
+        print(SEM, MG, SY)
+        school_year = SchoolYear.objects.filter(school_year=SY).first()
+        ratings = MGRating.objects.filter(school_year=school_year, group_title=MG, semester=SEM)
         return render(request, template_name="canvas/index.html", context={"ratings": ratings})
 
     @method_decorator(login_required(login_url="accounts:login"))
@@ -60,7 +63,7 @@ class CreateEval(View):
             messages.success(request, "Evaluation successfully created!")
             msg = EmailMessage('New Evaluation', 'You have a new evaluation in {school_year} - {group_title} - {semester}. You may now view it in the Faculty Evaluation'.format(school_year=school_year, group_title=group_title, semester=semester), to=[member.user.email])
             msg.send()
-            return redirect("canvas:index")
+            return redirect("canvas:index", SEM=semester, MG=group_title, SY=school_year)
         return redirect("/")
 
 class EditEval(View):
@@ -89,7 +92,10 @@ class EditEval(View):
             })
             return render(request, template_name="canvas/evaluation_form.html", context={"form":form})
         messages.error(request, f"ID {ID} does not exist!")
-        return redirect("canvas:index")
+        SEM = result.first().semester
+        MG = result.first().group_title
+        SY = result.first().school_year
+        return redirect("canvas:index", SEM, MG, SY)
 
     @method_decorator(login_required(login_url="accounts:login"))
     @method_decorator(admin_only)
@@ -99,7 +105,10 @@ class EditEval(View):
             form.save(commit=False)
             selected_mgrating = MGRating.objects.filter(id=ID)
             selected_mgrating.update(**form.cleaned_data)
-        return redirect("canvas:index")
+        SEM = form.cleaned_data.get("semester")
+        MG = form.cleaned_data.get("group_title")
+        SY = form.cleaned_data.get("school_year")
+        return redirect("canvas:index", SEM, MG, SY)
 
 @login_required(login_url="accounts:login")
 @admin_only
@@ -109,10 +118,14 @@ def DeleteEval(request, ID):
     faculty member using ID attribute in LMS's (Canvas) MGRating table.
     """
     result = MGRating.objects.filter(id=ID)
+    SEM = result.first().semester
+    MG = result.first().group_title
+    SY = result.first().school_year
     # Check if the performance evaluation already exist
     if result.exists():
         result.delete()
         messages.success(request, f"ID {ID} has been successfully removed")
-        return redirect("canvas:index")
+        return redirect("canvas:index", SEM, MG, SY)
     messages.error(request, f"ID {ID} does not exist!")
-    return redirect("canvas:index")
+    return redirect("canvas:index", SEM, MG, SY)
+    
