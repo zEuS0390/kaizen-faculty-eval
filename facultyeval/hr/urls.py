8412cd00a879e28298e63672828010293a3a6164
zEuS-0390 <zeusjamesbaltazar@gmail.com>
2022-05-04 15:36:00 +0800

    Update administrator HR app

--- facultyeval/hr/urls.py
+++ facultyeval/hr/urls.py
@@ -3,6 +3,8 @@ from .views import *
 
 app_name = "hr"
 urlpatterns = [
-    path("", Index.as_view(), name="index"),
-    path("evaluation-form-entry/<int:ID>/", CreateEval.as_view(), name="eval_entry")
+    path("list-of-criteria/", ListofCriteria, name="list_of_criteria"),
+    path("<slug:SEM>/<slug:SY>/", Index.as_view(), name="index"),
+    path("<slug:SEM>/<slug:SY>/hr-eval-scores/<int:ID>/", EvalScores.as_view(), name="hr_eval_scores"),
+    path("<slug:SEM>/<slug:SY>/hr-eval-scores/criterion-scores/<int:ID>/", CriterionScores.as_view(), name="criterion_scores")
 ]
