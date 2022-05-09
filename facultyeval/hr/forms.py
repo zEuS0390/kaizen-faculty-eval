bda896c241d3cce1a5d40ec8ff71ded87dba323c
zEuS-0390 <zeusjamesbaltazar@gmail.com>
2022-05-09 08:13:38 +0800

    Update administrator hr

--- facultyeval/hr/forms.py
+++ facultyeval/hr/forms.py
@@ -1,6 +1,18 @@
 from django import forms
 from .models import *
 
+class HRRatingForm(forms.ModelForm):
+
+    class Meta:
+        model = HRRating
+        fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super(HRRatingForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].required = True
+            self.fields[field].widget.attrs.update({"name": field, "class": "form-control"})
+
 class HRCriterionScoresForm(forms.ModelForm):
 
     class Meta:
--- facultyeval/hr/views.py
+++ facultyeval/hr/views.py
@@ -19,18 +19,17 @@ class Index(View):
         members = Member.objects.all()
         school_year = SchoolYear.objects.filter(school_year=SY).first()
         context = {"hr_data": [], "SEM": SEM, "SY":SY}
-        if not school_year is None:
-            for member in members:
-                result = HRRating.objects.filter(member=member, school_year=school_year, semester=SEM)
-                if result.exists():
-                    context["hr_data"].append([member, True])
-                    continue
-                context["hr_data"].append([member, False])
+        for member in members:
+            result = HRRating.objects.filter(member=member, school_year=school_year, semester=SEM)
+            if result.exists():
+                context["hr_data"].append([member, True])
+                continue
+            context["hr_data"].append([member, False])
         return render(request, template_name="hr/index.html", context=context)
 
     @method_decorator(login_required(login_url="accounts:login"))
     @method_decorator(admin_only)
-    def post(self, request, SEM, SY,):
+    def post(self, request, SEM, SY):
         return redirect("/")
 
 class EvalScores(View):
@@ -40,10 +39,6 @@ class EvalScores(View):
     def get(self, request, SEM, SY, ID):
         member = Member.objects.filter(id=ID).first()
         school_year = SchoolYear.objects.filter(school_year=SY).first()
-        hrrating = HRRating.objects.filter(member=member)
-        if not hrrating.exists():
-            hrrating_new = HRRating(member=member, school_year=school_year, semester=SEM)
-            hrrating_new.save()
         hrrating = HRRating.objects.filter(member=member).first()
         hrcriterionscores = HRCriterionScores.objects.filter(hrrating=hrrating).all()
         context = {
@@ -52,7 +47,7 @@ class EvalScores(View):
             "SY": SY,
             "SEM": SEM
         }
-        return render(request, template_name="hr/hrevalscores.html", context=context)
+        return render(request, template_name="hr/updatehrevalscores.html", context=context)
 
     @method_decorator(login_required(login_url="accounts:login"))
     @method_decorator(admin_only)
@@ -103,3 +98,43 @@ def ListofCriteria(request):
         "criteria": criteria
     }
     return render(request, template_name="hr/listofcriteria.html", context=context)
+
+class HRRatingEntry(View):
+
+    @method_decorator(login_required(login_url="accounts:login"))
+    @method_decorator(admin_only)
+    def get(self, request, SEM, SY):
+        form = HRRatingForm()
+        context = {"form": form}
+        return render(request, template_name="hr/hrrating.html", context=context)
+
+    @method_decorator(login_required(login_url="accounts:login"))
+    @method_decorator(admin_only)
+    def post(self, request, SEM, SY):
+        form = HRRatingForm(request.POST)
+        if form.is_valid():
+            hrrating = form.save(commit=False)
+            member = form.cleaned_data.get("member")
+            semester = form.cleaned_data.get("semester")
+            school_year = form.cleaned_data.get("school_year")
+            if HRRating.objects.filter(member=member, semester=semester, school_year=school_year).exists():
+                messages.error(request, "HR evaluation entry already exist!")
+                return redirect("hr:hr_rating_entry", SEM=semester, SY=school_year)
+            hrrating.save()
+            messages.success(request, "HR evaluation entry successfully created!")
+            return redirect("hr:index", SEM=semester, SY=school_year)
+        return redirect("hr:hr_rating_entry", SEM=SEM, SY=SY)
+
+@login_required(login_url="accounts:login")
+@admin_only
+def DeleteHRRating(request, SEM, SY, ID):
+    member = Member.objects.filter(id=ID).first()
+    school_year = SchoolYear.objects.filter(school_year=SY).first()
+    hrrating = HRRating.objects.filter(member=member, school_year=school_year, semester=SEM)
+    if hrrating.exists():
+        hrrating.delete()
+        messages.success(request, "HR evaluation entry was successfully deleted!")
+        return redirect("hr:index", SEM=SEM, SY=SY)
+    else:
+        messages.error(request, f"ID {ID} does not exist!")
+    return redirect("hr:index", SEM=SEM, SY=SY)
