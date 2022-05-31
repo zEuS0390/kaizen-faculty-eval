f86eabf2e3c1094ffd3e294b3968ef0ebb6c3684
zEuS-0390 <zeusjamesbaltazar@gmail.com>
2022-05-31 14:05:38 +0800

    Update member view

--- facultyeval/member/urls.py
+++ facultyeval/member/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
     path("", Home, name="home"),
     path("profile/", Profile, name="profile"),
     path("hr/", HR, name="hr"),
+    path("hr/eval-scores/<slug:SEM>/<slug:SY>/", HREvalScores, name="hr_eval_scores"),
     path("aiv/", AIV, name="aiv"),
+    path("aiv/eval-scores/<slug:SEM>/<slug:SY>", AIVEvalScores, name="aiv_eval_scores"),
     path("lms/", LMS, name="lms"),
     path("about/", About, name="about")
 ]
--- facultyeval/member/views.py
+++ facultyeval/member/views.py
@@ -5,6 +5,8 @@ from django.utils.decorators import method_decorator
 from .decorators import member_only
 from administrator.models import ActivityLogs
 from accounts.models import Member
+from hr.models import *
+from aiv.models import *
 
 # Create your views here.
 @login_required(login_url="accounts:login")
@@ -22,12 +24,50 @@ def Profile(request):
 @login_required(login_url="accounts:login")
 @member_only
 def HR(request):
-    return render(request, template_name="member/hr.html", context={})
+    member = Member.objects.filter(user=request.user).first()
+    hrratings = HRRating.objects.filter(member=member).all()
+    context = {
+        "hrratings": hrratings
+    }
+    return render(request, template_name="member/hr.html", context=context)
+
+@login_required(login_url="accounts:login")
+@member_only
+def HREvalScores(request, SEM, SY):
+    member = Member.objects.filter(user=request.user).first()
+    school_year = SchoolYear.objects.filter(school_year=SY).first()
+    hrrating = HRRating.objects.filter(member=member, school_year=school_year, semester=SEM).first()
+    hrcriterionscores = HRCriterionScores.objects.filter(hrrating=hrrating).all()
+    context = {
+            "hrcriterionscores": hrcriterionscores,
+            "SY": SY,
+            "SEM": SEM
+    }
+    return render(request, template_name="member/hrevalscores.html", context=context)
 
 @login_required(login_url="accounts:login")
 @member_only
 def AIV(request):
-    return render(request, template_name="member/aiv.html", context={})
+    member = Member.objects.filter(user=request.user).first()
+    aivratings = AIVRating.objects.filter(member=member).all()
+    context = {
+        "aivratings": aivratings
+    }
+    return render(request, template_name="member/aiv.html", context=context)
+
+@login_required(login_url="accounts:login")
+@member_only
+def AIVEvalScores(request, SEM, SY):
+    member = Member.objects.filter(user=request.user).first()
+    school_year = SchoolYear.objects.filter(school_year=SY).first()
+    aivrating = AIVRating.objects.filter(member=member, school_year=school_year, semester=SEM).first()
+    aivcriterionscores = AIVCriterionScores.objects.filter(aivrating=aivrating).all()
+    context = {
+            "aivcriterionscores": aivcriterionscores,
+            "SY": SY,
+            "SEM": SEM
+    }
+    return render(request, template_name="member/aivevalscores.html", context=context)
 
 @login_required(login_url="accounts:login")
 @member_only
