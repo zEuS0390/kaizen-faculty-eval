e4ba5472f2c95ef9797d4de4a961295b48692b82
zEuS-0390 <zeusjamesbaltazar@gmail.com>
2022-06-02 16:22:09 +0800

    Add staticfiles to the STATIC_ROOT settings

--- facultyeval/facultyeval/settings.py
+++ facultyeval/facultyeval/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "staticfiles")
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
