7de7befa9e7a74f19042438e03b9660aac6206af
zEuS-0390 <zeusjamesbaltazar@gmail.com>
2022-06-02 22:00:50 +0800

    Fix static path

--- facultyeval/facultyeval/settings.py
+++ facultyeval/facultyeval/settings.py
@@ -133,10 +133,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, 'static/')
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
