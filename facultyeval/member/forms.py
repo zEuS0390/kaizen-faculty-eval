c8594b06ee0c62a2c9fca49152b42cfdc644be5c
zEuS-0390 <zeusjamesbaltazar@gmail.com>
2022-06-07 08:36:03 +0800

    Add bootstrap wrapper for django profile form

--- facultyeval/member/forms.py
+++ facultyeval/member/forms.py
@@ -8,8 +8,19 @@ class EditUserForm(forms.ModelForm):
         model = User
         fields = ["first_name", "last_name", "email"]
 
+    def __init__(self, *args, **kwargs):
+        super(EditUserForm, self).__init__(*args, **kwargs)
+        self.fields["first_name"].widget.attrs.update({"class": "form-control"})
+        self.fields["last_name"].widget.attrs.update({"class": "form-control"})
+        self.fields["email"].widget.attrs.update({"class": "form-control"})
+
 class EditMemberForm(forms.ModelForm):
 
     class Meta:
         model = Member
         fields = ["middle_name", "image"]
+
+    def __init__(self, *args, **kwargs):
+        super(EditMemberForm, self).__init__(*args, **kwargs)
+        self.fields["middle_name"].widget.attrs.update({"class":"form-control"})
+        self.fields["image"].widget.attrs.update({"type": "file", "class":"form-control"})
